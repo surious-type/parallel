43ff2bbfa4135e27ac163f10aec1cc86ca7b6a8e
surious-type <daniil_vasilenko757@mail.ru>
2026-03-31 14:59:04 +0300

    version time calculate for every relax

--- 1/var03.foromp.c
+++ 1/var03.foromp.c
@@ -19,25 +19,29 @@ void verify();
 int main(int an, char **as)
 {
 	int it;
+    double relax_total = 0.0;
 	init();
 
 	for(it=1; it<=itmax; it++)
 	{
 		eps = 0.;
+        double t0 = omp_get_wtime();
 		relax();
-		printf( "it=%4i   eps=%f\n", it,eps);
+        double t1 = omp_get_wtime();
+        relax_total += (t0 - t1);
+		printf( "it=%4i   eps=%f relax_time=%f\n", it, eps, t0 - t1);
 		if (eps < maxeps) break;
 	}
+
+    printf("Time=%f\n", relax_total);
 
 	verify();
 
 	return 0;
 }
 
-
 void init()
 {
-    #pragma omp parallel for collapse(3) private(i,j,k) schedule(static)
 	for(k=0; k<=N-1; k++)
 	for(j=0; j<=N-1; j++)
 	for(i=0; i<=N-1; i++)
@@ -49,37 +53,41 @@ void init()
 
 void relax()
 {
-	for(k=3; k<=N-4; k++)
+    #pragma omp parallel
     {
-        #pragma omp parallel for collapse(2) private(i,j) schedule(static)
-        for(j=1; j<=N-2; j++)
-        for(i=1; i<=N-2; i++)
+
+        for(k=3; k<=N-4; k++)
         {
-            A[i][j][k] = (A[i][j][k-1]+A[i][j][k+1]+A[i][j][k-2]+A[i][j][k+2]+A[i][j][k-3]+A[i][j][k+3])/6.f;
+            #pragma omp for collapse(2) private(i,j) schedule(static)
+            for(j=1; j<=N-2; j++)
+            for(i=1; i<=N-2; i++)
+            {
+                A[i][j][k] = (A[i][j][k-1]+A[i][j][k+1]+A[i][j][k-2]+A[i][j][k+2]+A[i][j][k-3]+A[i][j][k+3])/6.f;
+            }
+        }	
+
+        for(i=3; i<=N-4; i++)
+        {
+            #pragma omp for collapse(2) private(k,j) schedule(static)
+            for(k=1; k<=N-2; k++)
+            for(j=1; j<=N-2; j++)
+            {
+                A[i][j][k] = (A[i-1][j][k]+A[i+1][j][k]+A[i-2][j][k]+A[i+2][j][k]+A[i-3][j][k]+A[i+3][j][k])/6.;
+            }
         }
-    }	
 
-	for(i=3; i<=N-4; i++)
-    {
-        #pragma omp parallel for collapse(2) private(k,j) schedule(static)
-	    for(k=1; k<=N-2; k++)
-	    for(j=1; j<=N-2; j++)
-	    {
-		    A[i][j][k] = (A[i-1][j][k]+A[i+1][j][k]+A[i-2][j][k]+A[i+2][j][k]+A[i-3][j][k]+A[i+3][j][k])/6.;
-	    }
-    }
-
-	for(j=3; j<=N-4; j++)
-    {
-        #pragma omp parallel for collapse(2) private(k,i) reduction(max:eps) schedule(static)
-	    for(k=1; k<=N-2; k++)
-	    for(i=1; i<=N-2; i++)
-	    {
-		    float e;
-		    e=A[i][j][k];
-		    A[i][j][k] =(A[i][j-1][k]+A[i][j+1][k]+A[i][j-2][k]+A[i][j+2][k]+A[i][j-3][k]+A[i][j+3][k])/6.;
-		    eps=Max(eps,fabs(e-A[i][j][k]));
-	    }
+        for(j=3; j<=N-4; j++)
+        {
+            #pragma omp for collapse(2) private(k,i) reduction(max:eps) schedule(static)
+            for(k=1; k<=N-2; k++)
+            for(i=1; i<=N-2; i++)
+            {
+                float e;
+                e=A[i][j][k];
+                A[i][j][k] =(A[i][j-1][k]+A[i][j+1][k]+A[i][j-2][k]+A[i][j+2][k]+A[i][j-3][k]+A[i][j+3][k])/6.;
+                eps=Max(eps,fabs(e-A[i][j][k]));
+            }
+        }
     }
 }
 
