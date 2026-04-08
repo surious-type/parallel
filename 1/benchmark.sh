b795f5b8db84b7306175f36582bcf73b75e75bab
surious-type <daniil_vasilenko757@mail.ru>
2026-04-08 18:46:53 +0300

    fix benchmark for k-10

--- 1/benchmark.sh
+++ 1/benchmark.sh
@@ -7,35 +7,53 @@ FLAG_OPTIMIZE=${3:-"-O2"}
 RUNS=5
 
 N_BASE=$((2 ** 6)) # 64
-# MEM=$(awk '/MemAvailable/ {print $2}' /proc/meminfo)
-# MEM_HALF=$((MEM / 2))
-# K=$(awk -v m="$MEM_HALF" -v nbase="$N_BASE" 'BEGIN {printf "%d", ((m*1024/8)^(1/3))/nbase}')
 
 OUT_DIR="results"
 mkdir -p $OUT_DIR
+mkdir -p bin
 
-if [ "$CC" = "xlc_r" ]; then
-    FLAGS="-qsmp=omp ${FLAG_OPTIMIZE}"
-else
-    FLAGS="-fopenmp ${FLAG_OPTIMIZE}"
-fi
+# --- выбор OpenMP флага ---
+case "$CC" in
+gcc)
+    OMP_FLAG="-fopenmp"
+    ;;
+icc)
+    OMP_FLAG="-qopenmp"
+    ;;
+pgcc)
+    OMP_FLAG="-mp"
+    ;;
+xlc_r)
+    OMP_FLAG="-qsmp=omp"
+    ;;
+*)
+    echo "Unknown compiler: $CC"
+    exit 1
+    ;;
+esac
 
-MAX_THREADS=${LSB_DJOB_NUMPROC:-$(nproc)}
+FLAGS="$OMP_FLAG $FLAG_OPTIMIZE"
 
+MAX_THREADS=$(nproc)
+
+echo "Compiler=$CC"
+echo "FLAGS=$FLAGS"
 echo "MAX_THREADS=$MAX_THREADS"
 
-NAME="$(basename ${SRC%.c}_${FLAG_OPTIMIZE})"
+NAME="$(basename ${SRC%.c}_${CC}_${FLAG_OPTIMIZE})"
 CSV="${OUT_DIR}/${NAME}.csv"
 echo "threads,run,time,N" >$CSV
 
-for ((K = 0; K < 2; K++)); do
+for ((K = 0; K < 4; K++)); do
     N=$((N_BASE * (2 ** K) + 2))
-    NAME_N="$(basename ${SRC%.c}_${FLAG_OPTIMIZE}_${N})"
+
+    NAME_N="$(basename ${SRC%.c}_${CC}_${FLAG_OPTIMIZE}_${N})"
     BIN="bin/${NAME_N}"
+
     echo "compile ${NAME_N}"
 
     if ! $CC $FLAGS $SRC -DN=$N -o $BIN -lm; then
-        echo "compile fail ${FLAG_OPTIMIZE}_${N}"
+        echo "compile fail ${CC}_${FLAG_OPTIMIZE}_${N}"
         continue
     fi
 
@@ -45,5 +63,6 @@ for ((K = 0; K < 2; K++)); do
             echo "$t,$r,$TIME,$N" >>"$CSV"
         done
     done
-    echo $N
+
+    echo "Done N=$N"
 done
