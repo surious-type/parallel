d34f90ef658ddbeb084afca96231abf81d507c61
surious-type <daniil_vasilenko757@mail.ru>
2026-04-14 01:36:54 +0300

    correct task

--- src/var03.taskomp.c
+++ src/var03.taskomp.c
@@ -8,7 +8,7 @@
 #ifndef N
 	#define N (2 * 2 * 2 * 2 * 2 * 2 + 2)
 #endif
-#ifndef N
+#ifndef THREADS
 	#define THREADS 1
 #endif
 
@@ -53,9 +53,9 @@ int main(int an, char **as)
 
 void init()
 {
-	for (int k = 0; k <= N - 1; k++)
+	for (int i = 0; i <= N - 1; i++)
 		for (int j = 0; j <= N - 1; j++)
-			for (int i = 0; i <= N - 1; i++)
+			for (int k = 0; k <= N - 1; k++)
 			{
 				if (i == 0 || i == N - 1 || j == 0 || j == N - 1 || k == 0 || k == N - 1)
 					A[i][j][k] = 0.0f;
@@ -66,26 +66,45 @@ void init()
 
 void relax()
 {
-	const int IB = 32;
-	const int JB = 32;
-	const int KB = 32;
+	int block = (N - 2) / THREADS * 2; // * 2, как минимум 2 задачи на нить
+
+	if (block <= 8)
+		block = 8;
+	else if (block <= 16)
+		block = 16;
+	else if (block <= 32)
+		block = 32;
+	else
+		block = 64;
+
+	// фаза 1: хорошая локальность - последовательная память
+	int IB1 = block;
+	int JB1 = block;
+
+	// фаза 2: плохая локальность, уменьшаем рабочий набор - N прыжки по памяти
+	int KB2 = block > 8 ? block / 2 : 8;
+	int JB2 = block > 8 ? block / 2 : 8;
+
+	// фаза 3: средняя локальность - N * N - прыжки по памяти
+	int KB3 = block;
+	int IB3 = block;
 
 #pragma omp parallel
 	{
 #pragma omp single
 		{
 #pragma omp taskgroup
-			for (int jb = 1; jb <= N - 2; jb += JB)
-				for (int ib = 1; ib <= N - 2; ib += IB)
+			for (int ib = 1; ib <= N - 2; ib += IB1)
+				for (int jb = 1; jb <= N - 2; jb += JB1)
 				{
 					int j0 = jb;
-					int j1 = Min(jb + JB - 1, N - 2);
+					int j1 = Min(jb + JB1 - 1, N - 2);
 					int i0 = ib;
-					int i1 = Min(ib + IB - 1, N - 2);
+					int i1 = Min(ib + IB1 - 1, N - 2);
 #pragma omp task firstprivate(j0, j1, i0, i1)
 					{
-						for (int j = j0; j <= j1; j++)
-							for (int i = i0; i <= i1; i++)
+						for (int i = i0; i <= i1; i++)
+							for (int j = j0; j <= j1; j++)
 								for (int k = 3; k <= N - 4; k++)
 								{
 									A[i][j][k] =
@@ -103,17 +122,17 @@ void relax()
 #pragma omp single
 		{
 #pragma omp taskgroup
-			for (int kb = 1; kb <= N - 2; kb += KB)
-				for (int jb = 1; jb <= N - 2; jb += JB)
+			for (int jb = 1; jb <= N - 2; jb += JB2)
+				for (int kb = 1; kb <= N - 2; kb += KB2)
 				{
 					int k0 = kb;
-					int k1 = Min(kb + KB - 1, N - 2);
+					int k1 = Min(kb + KB2 - 1, N - 2);
 					int j0 = jb;
-					int j1 = Min(jb + JB - 1, N - 2);
+					int j1 = Min(jb + JB2 - 1, N - 2);
 #pragma omp task firstprivate(k0, k1, j0, j1)
 					{
-						for (int k = k0; k <= k1; k++)
-							for (int j = j0; j <= j1; j++)
+						for (int j = j0; j <= j1; j++)
+							for (int k = k0; k <= k1; k++)
 								for (int i = 3; i <= N - 4; i++)
 								{
 									A[i][j][k] =
@@ -131,20 +150,20 @@ void relax()
 		{
 #pragma omp taskgroup
 			{
-				for (int kb = 1; kb <= N - 2; kb += KB)
-					for (int ib = 1; ib <= N - 2; ib += IB)
+				for (int ib = 1; ib <= N - 2; ib += IB3)
+					for (int kb = 1; kb <= N - 2; kb += KB3)
 					{
 						int k0 = kb;
-						int k1 = Min(kb + KB - 1, N - 2);
+						int k1 = Min(kb + KB3 - 1, N - 2);
 						int i0 = ib;
-						int i1 = Min(ib + IB - 1, N - 2);
+						int i1 = Min(ib + IB3 - 1, N - 2);
 
 #pragma omp task firstprivate(k0, k1, i0, i1) shared(eps)
 						{
 							float local_max = 0.0f;
 
-							for (int k = k0; k <= k1; k++)
-								for (int i = i0; i <= i1; i++)
+							for (int i = i0; i <= i1; i++)
+								for (int k = k0; k <= k1; k++)
 									for (int j = 3; j <= N - 4; j++)
 									{
 										float oldv = A[i][j][k];
@@ -175,8 +194,8 @@ void verify()
 	s = 0.0f;
 
 	for (int k = 0; k <= N - 1; k++)
-		for (int j = 0; j <= N - 1; j++)
-			for (int i = 0; i <= N - 1; i++)
+		for (int i = 0; i <= N - 1; i++)
+			for (int j = 0; j <= N - 1; j++)
 			{
 				s = s + A[i][j][k] * (i + 1) * (j + 1) * (k + 1) / (N * N * N);
 			}
